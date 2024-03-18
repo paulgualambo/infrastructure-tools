2e155a3371aa40f6d96655094db025fc7b9ccbd8
Paul Gualambo <paul.gualambo@gmail.com>
2024-03-18 01:36:47 -0500

    Update config_install_software_dev.sh

--- linux/config_install_software_dev.sh
+++ linux/config_install_software_dev.sh
@@ -2,16 +2,45 @@
 #
 # This script is used to install software development tools.
 # ubuntu debian
+# use 
+##
+## wget -O - https://raw.githubusercontent.com/paulgualambo/infrastructure-tools/main/linux/config_install_software_dev.sh | bash -s $USERNAME
+##
 
 USERNAME=${1}
+
+#https://linuxize.com/post/how-to-add-user-to-sudoers-in-ubuntu/
+sudo usermod -aG sudo $USERNAME
+sudo whoami # return root then the user is as root
+
+#Config timezone America/Lima
+sudo timedatectl set-timezone "America/Lima"
 
 sudo apt-get install -y git
 sudo apt-get install -y net-tools
 
 #Install docker
-
-#Config timezone America/Lima
-sudo timedatectl set-timezone "America/Lima"
+##remove docker
+#DEBIAN
+sudo apt-get remove docker docker-engine docker.io containerd runc
+sudo apt-get update
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+##install docker
+sudo apt-get update
+sudo apt-get install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --batch --yes --no-tty --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+sudo docker --version
+sudo usermod -aG docker $USERNAME
+sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
 
 # Registro de bash
 # Crea un nuevo usuario si aún no existe
@@ -21,7 +50,13 @@ id -u $USERNAME &>/dev/null || useradd -m -s /bin/bash  $USERNAME
 HOME_DIR="/home/$USERNAME"
 
 # Clona Bash-it en el directorio del nuevo usuario
-git clone --depth=1 https://github.com/Bash-it/bash-it.git $HOME_DIR/.bash_it
+if [ -d "$HOME_DIR/.bash_it" ]; then
+  pushd $HOME_DIR/.bash_it
+  git pull
+  popd
+else
+  git clone --depth=1 https://github.com/Bash-it/bash-it.git $HOME_DIR/.bash_it
+fi
 
 # Ejecuta el script de instalación de Bash-it como el nuevo usuario
 runuser -l $USERNAME -c 'yes | '$HOME_DIR'/.bash_it/install.sh'

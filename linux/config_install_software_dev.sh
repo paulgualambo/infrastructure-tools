5d970f08b56a14aead79f3b9041327ee10161a49
Paul Gualambo <paul.gualambo@gmail.com>
2024-03-18 01:27:27 -0500

    Update config_install_software_dev.sh

--- linux/config_install_software_dev.sh
+++ linux/config_install_software_dev.sh
@@ -2,36 +2,33 @@
 #
 # This script is used to install software development tools.
 # ubuntu debian
-sudo apt install -y git
-sudo apt install -y net-tools
+
+USERNAME=${1}
+
+sudo apt-get install -y git
+sudo apt-get install -y net-tools
 
 #Install docker
-
-#Ingresar con el usuario con el quien se va a trabajar
-
-su <user>
-
-# centos amzn fedora 
-sudo yum install -y git
-sudo yum install -y nc nmap
 
 #Config timezone America/Lima
 sudo timedatectl set-timezone "America/Lima"
 
-#fix bash sh
-chsh -s /bin/bash
+# Registro de bash
+# Crea un nuevo usuario si aún no existe
+id -u $USERNAME &>/dev/null || useradd -m -s /bin/bash  $USERNAME
 
-# Install bash-it
-cd ~
-git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
-~/.bash_it/install.sh
-source ~/.bashrc
-sed -i 's/'$BASH_IT_THEME'/atomic/g' ~/.bashrc
-bash_it reload
+# Cambia a la home del nuevo usuario
+HOME_DIR="/home/$USERNAME"
 
-#https://ohmybash.nntoan.com/
-bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
+# Clona Bash-it en el directorio del nuevo usuario
+git clone --depth=1 https://github.com/Bash-it/bash-it.git $HOME_DIR/.bash_it
 
-sh -c “$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)”
+# Ejecuta el script de instalación de Bash-it como el nuevo usuario
+runuser -l $USERNAME -c 'yes | '$HOME_DIR'/.bash_it/install.sh'
 
-bash_it reload
+# Cambia el tema de Bash-it a 'atomic' para el nuevo usuario
+sed -i "s/export BASH_IT_THEME='.*'/export BASH_IT_THEME='atomic'/g" $HOME_DIR/.bashrc
+
+# Asegúrate de que los archivos tienen al nuevo usuario como propietario
+chown -R $USERNAME:$USERNAME $HOME_DIR/.bash_it
+chown $USERNAME:$USERNAME $HOME_DIR/.bashrc
